db1f044851f2498ea83332b43b97ba6b43e55bc1
Alex Howard <thezanke@gmail.com>
2021-11-30 14:53:29 -0500

    colors are hard

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -6,7 +6,7 @@ export const Logo = () => {
     <Typography
       variant="h3"
       my={5}
-      color={theme.palette.primary.main}
+      color={theme.palette.primary.light}
       align="center"
       fontFamily="'M PLUS 1 Code', sans-serif;"
     >
