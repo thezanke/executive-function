c3ee31dc0633d07c00ac0a4f46006401ecbfcab2
Alex Howard <thezanke@gmail.com>
2021-11-30 15:25:47 -0500

    try supporting touch

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import React from "react";
+import { isMobile } from "react-device-detect";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from "react-dnd-touch-backend";
 import { MainLayout } from "./MainLayout";
 import { theme } from "./theme";
 
@@ -11,7 +13,7 @@ const App = () => {
     <React.StrictMode>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <DndProvider backend={HTML5Backend}>
+        <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
           <MainLayout />
         </DndProvider>
       </ThemeProvider>
